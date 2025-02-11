bb13ebee36ff9a4a47cf0b3f70bd991292d54863
AlexFullmoon <alex.fullmoon@moonlightwell.ru>
2025-02-11 16:48:31 +0300

    Added bitwarden cli unlock to powershell profile

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,5 +2,8 @@ Invoke-Expression (&starship init powershell)
 
 Set-Location C:\Users\Fullmoon
 
+# Unlocking Bitwarden CLI
+Function bw-unlock {$env:BW_SESSION=(bw unlock --raw)}
+
 # Keep at the end of config
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
